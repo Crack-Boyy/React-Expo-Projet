cea1024db20a756bbe48c73de33e969dc8780250
Crack-Boyy <mayotyohan@gmail.com>
2025-09-02 13:57:57 +0200

    first update

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import Human from "@/components/Human/Human";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+export default function HomeScreen() {
+return (
+<ThemedView style={{ flex: 1, justifyContent: "center", alignItems: "center"}} >
+<Human />
+<Human />
+<ThemedText> 
+Fin de l écran
+</ThemedText>
+</ThemedView>
+);
+}
